892bc7c95f274e3a55461df86cd189e81f351dc2
Hasan Salam <Hasan.sal1029@gmail.com>
2016-11-04 10:08:59 +0000

    Add files via upload
    Code is poorly done, will re-made to an advanced standard

---
+++ Little Man Computer (Crap).py
@@ -0,0 +1,273 @@
+#Little Man Computer
+#Project By Hasan Salam
+#03/11/2016
+import time
+
+accumulator=0
+count=0
+print ("LOAD 'ONE' into the ACCUMLATOR")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+accumlator=1
+time.sleep(1)
+print ("ACCUMULATOR is '1'")
+time.sleep(1)
+print ("STORE COUNT")
+time.sleep(1)
+print ("Find 'COUNT' from data")
+time.sleep(1)
+print ("'COUNT' is DATA '0'")
+count=0
+time.sleep(1)
+print ("'COUNT' is '0'")
+time.sleep(1)
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+accumulator=count
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '1'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+print ("'LOOPTOP' is here.")
+time.sleep(1)
+print ("LOAD 'count'")
+time.sleep(1)
+print ("ADD 'ONE'")
+time.sleep(1)
+print ("Find 'ONE' from data")
+time.sleep(1)
+print ("'ONE' is DATA '1'")
+count=count+1
+time.sleep(1)
+print ("'COUNT' is '2'")
+time.sleep(1)
+print ("STORE 'COUNT'")
+time.sleep(1)
+print ("SUBTRACT 'TEN'")
+time.sleep(1)
+accumulator=accumulator-10
+print ("Number:", (accumulator))
+if accumulator>0:
+    print ("BRANCH to ENDLOOP")
+else:
+    print ("BRANCH to LOOPTOP")
+
+
+
+
+
+
+
