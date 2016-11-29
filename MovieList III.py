8c26a945465f60e85a8695abc006933af2695928
Hasan Salam <Hasan.sal1029@gmail.com>
2016-11-29 20:59:18 +0000

    Film List one

---
+++ MovieList III.py
@@ -0,0 +1,10 @@
+File = open('MovieList.txt', 'r')
+Film_Amounts=File.readlines()
+print (Film_Amounts)
+
+with open('MovieList.txt') as File:
+    Films = sum(1 for line in File if line.rstrip('\n'))
+    print (Films)
+
+
+
